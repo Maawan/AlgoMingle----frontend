085a83270ac2cbbeed3548b076d0c892e5381a0c
Mohd Mawan Ahmad <maawan18@gmail.com>
2023-12-26 15:37:06 +0530

    relf

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -74,7 +74,7 @@ const Register = () => {
 
 
   return (
-    <div className="w-full flex justify-center bg-[url('./wal/wal1.png')]">
+    <div className="w-full flex justify-center bg-[url('/wal/wal1.png')]">
       <div className="w-[400px]  flex justify-center items-center m-4 border-1 bg-white rounded-lg shadow-orange-300 shadow-md ">
         <div className="w-full flex flex-col my-10 items-center">
           <img src="./logos/logo14.png" alt="" className="h-[70px] w-[70px]" />
